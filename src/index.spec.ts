2fdb24e7ca84699a5d1b92685fbc4cef9b569199
Zhao DAI <zddai@amazon.com>
2019-12-27 16:41:12 +0000

    Build skeleton.

---
+++ src/index.spec.ts
@@ -0,0 +1,5 @@
+import { add } from './';
+
+test('should return expected result', () => {
+  expect(add(1, 2)).toBe(3);
+});
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Add two numbers.
+ * @param a A number
+ * @param b Another number
+ */
+export function add(a: number, b: number) {
+  return a + b;
+}
